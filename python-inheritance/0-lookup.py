db39d45d9e233a2419ea956def71870dab8eb0e6
Alexander1455 <alexander21111121@gmail.com>
2024-01-14 19:47:02 -0500

    fix error

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -8,4 +8,4 @@ available attributes and methods of an object
 
 def lookup(obj):
     """Returns the list of available attributes and methods of an object"""
-    return dir(obj)
+    return [atributo for atributo in dir(obj)]
